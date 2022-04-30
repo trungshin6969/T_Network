c2995fce9ee2071837b12e6c60a9207b3255dc0d
trungshin6969 <trungnpvgcs18890@fpt.edu.vn>
2022-04-30 23:16:19 +0700

    fix private_route

--- src/components/RequireAuth/RequireAuth.jsx
+++ src/components/RequireAuth/RequireAuth.jsx
@@ -3,10 +3,10 @@ import { Navigate } from "react-router-dom";
 
 const RequireAuth = ({ children }) => {
   const auth = useSelector((state) => state.auth.login?.currentUser);
-  return auth === null ? (
-    <Navigate to="/"/>
-  ) : (
+  return auth ? (
     children
+  ) : (
+    <Navigate to="/"/>
   );
 };
 
